a76671839a4a338e2c9b367cec5e67872e8468ab
Rohit Kumar <69639713+Rohit26071998@users.noreply.github.com>
2024-10-19 16:49:27 +0000

    variable in JS

---
+++ basics/dataType.js
@@ -0,0 +1,14 @@
+// Data Tpye in JS
+// number
+// bigint
+// string
+// boolean
+// null --> standalone value
+// undefined
+// symbol
+// Object
+
+let name = "Rohit"
+let age = 25
+let married = false
+
