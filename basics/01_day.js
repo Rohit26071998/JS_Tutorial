802db24643523a1a8ded52dd1b4b301bfb203c1b
Rohit Kumar <69639713+Rohit26071998@users.noreply.github.com>
2025-06-12 04:54:46 +0000

    Done variables and const

--- basics/01_day.js
+++ basics/01_day.js
@@ -3,3 +3,21 @@
  2. var
  3. const
 */
+// const empId = "SYS2584"
+// let name = "Rohit Kumar"
+// var email = "rohit.kumar4@idemia.com"
+// let state
+
+// empId = "SYS2576" not allowed
+
+// name = "Steve"
+// email = "steve@gmail.com"
+// console.table([empId,name,email,state])
+// ┌─────────┬───────────────────┐
+// │ (index) │ Values            │
+// ├─────────┼───────────────────┤
+// │ 0       │ 'SYS2584'         │
+// │ 1       │ 'Steve'           │
+// │ 2       │ 'steve@gmail.com' │
+// │ 3       │ undefined         │
+// └─────────┴───────────────────┘
